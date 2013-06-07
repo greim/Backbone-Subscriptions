652ff5b4aecb065a1435a83a93e01a852cbbe60c
greim <gr@wayin.com>
2013-06-07 12:12:21 -0600

    Converted indentation to two spaces per indent level

--- backbone-subscriptions.js
+++ backbone-subscriptions.js
@@ -30,94 +30,94 @@ SOFTWARE.
 
 (function(){
 
+  /*
+   * If AMD is available, use it.
+   */
+  var isAmd = typeof window.define === "function" && window.define.amd;
+  var define = isAmd ? window.define : function(list, cb){
+    cb(jQuery, Backbone, _, window.await);
+  };
+
+  define([
+    'jquery',
+    'backbone',
+    'underscore',
+    'await'
+  ],function(
+    $,
+    Backbone,
+    _,
+    await
+  ){
+
     /*
-     * If AMD is available, use it.
+     * This is a bit of voodoo to ensure that subscribing
+     * views are reachable through the DOM.
      */
-    var isAmd = typeof window.define === "function" && window.define.amd;
-    var define = isAmd ? window.define : function(list, cb){
-        cb(jQuery, Backbone, _, window.await);
+    var setElement = Backbone.View.prototype.setElement;
+    Backbone.View.prototype.setElement = function(){
+      var result = setElement.apply(this, arguments);
+      if (this.subscriptions) {
+        this.el.view = this;
+        this.el.className += ' subscriber';
+      }
+      return result;
     };
 
-    define([
-        'jquery',
-        'backbone',
-        'underscore',
-        'await'
-    ],function(
-        $,
-        Backbone,
-        _,
-        await
-    ){
+    if (!await) {
+      await = function(){};
+      await.all = function(){};
+    }
 
-        /*
-         * This is a bit of voodoo to ensure that subscribing
-         * views are reachable through the DOM.
-         */
-        var setElement = Backbone.View.prototype.setElement;
-        Backbone.View.prototype.setElement = function(){
-            var result = setElement.apply(this, arguments);
-            if (this.subscriptions) {
-                this.el.view = this;
-                this.el.className += ' subscriber';
-            }
-            return result;
-        };
-
-        if (!await) {
-            await = function(){};
-            await.all = function(){};
+    /*
+     * Returns a list of elements by class name. Uses native
+     * DOM query functionality for maximum performance.
+     */
+    var elementListByClass = (function(){
+      var lists = {}; // this is the cache
+      var slice = [].slice;
+      var useFallback = typeof document.getElementsByClassName !== 'function';
+      return function(className){
+        if (useFallback) {
+          return slice.call(document.querySelectorAll('.'+className));
+        } else {
+          if (!lists[className]) {
+            // gebcn returns a "live" list, so it will be automatically
+            // updated by the browser from here on out
+            lists[className] = document.getElementsByClassName(className);
+          }
+          return slice.call(lists[className]);
         }
+      };
+    })();
 
-        /*
-         * Returns a list of elements by class name. Uses native
-         * DOM query functionality for maximum performance.
-         */
-        var elementListByClass = (function(){
-            var lists = {}; // this is the cache
-            var slice = [].slice;
-            var useFallback = typeof document.getElementsByClassName !== 'function';
-            return function(className){
-                if (useFallback) {
-                    return slice.call(document.querySelectorAll('.'+className));
-                } else {
-                    if (!lists[className]) {
-                        // gebcn returns a "live" list, so it will be automatically
-                        // updated by the browser from here on out
-                        lists[className] = document.getElementsByClassName(className);
-                    }
-                    return slice.call(lists[className]);
-                }
-            };
-        })();
+    /**
+     * Publish/subscribe for Backbone views.
+     */
+    Backbone.Subscriptions = {
+      publish: function(channel){
+        var args = Array.prototype.slice.call(arguments, 1);
+        var liveElements = elementListByClass('subscriber');
+        var proms = _(liveElements).filter(function(el){
+          return el.view
+            && el.view.subscriptions
+            && el.view.subscriptions[channel];
+        });
+        proms = _(proms).map(function(el){
+          var view = el.view;
+          var subs = view.subscriptions;
+          var methodName = subs[channel];
+          var prom = view[methodName].apply(view, args);
+          if (!(prom instanceof await)) {
+            prom = await();
+          }
+          return prom;
+        });
+        return await.all(proms);
+      }
+    }
+  });
 
-        /**
-         * Publish/subscribe for Backbone views.
-         */
-        Backbone.Subscriptions = {
-            publish: function(channel){
-                var args = Array.prototype.slice.call(arguments, 1);
-                var liveElements = elementListByClass('subscriber');
-                var proms = _(liveElements).filter(function(el){
-                    return el.view
-                        && el.view.subscriptions
-                        && el.view.subscriptions[channel];
-                });
-                proms = _(proms).map(function(el){
-                    var view = el.view;
-                    var subs = view.subscriptions;
-                    var methodName = subs[channel];
-                    var prom = view[methodName].apply(view, args);
-                    if (!(prom instanceof await)) {
-                        prom = await();
-                    }
-                    return prom;
-                });
-                return await.all(proms);
-            }
-        }
-    });
-
-    return Backbone.Subscriptions;
+  return Backbone.Subscriptions;
 
 })();
